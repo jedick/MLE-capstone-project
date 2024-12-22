070fa0a7b309e3948a431cd0ac98b75e3b236a1f
jedick <j3ffdick@gmail.com>
2024-12-22 14:58:54 +0800

    Add notebook on checkpoints and rationale weight

--- model/script/xtrain.sh
+++ model/script/xtrain.sh
@@ -26,65 +26,64 @@ train_and_predict () {
     ckpt=/home/multivers/checkpoints/xtrain$3_$last.ckpt
   fi
 
-#  echo "python script/train_target.py --dataset $2 --gpus=1 --epochs=$3 --ckpt=$ckpt --gradient_checkpointing"
-#  time python script/train_target.py --dataset $2 --gpus=1 --epochs=$3 --ckpt=$ckpt --gradient_checkpointing
+  echo "python script/train_target.py --dataset $2 --gpus=1 --epochs=$3 --ckpt=$ckpt --gradient_checkpointing"
+  time python script/train_target.py --dataset $2 --gpus=1 --epochs=$3 --ckpt=$ckpt --gradient_checkpointing
   echo "outputting predictions to ../predictions/xtrain$3_$num_$4.jsonl"
   python multivers/predict.py \
-    #--checkpoint_path checkpoints_user/$2/checkpoint/last.ckpt \
-    --checkpoint_path /home/multivers/checkpoints/xtrain$3/xtrain$3_$num.ckpt \
+    --checkpoint_path checkpoints_user/$2/checkpoint/last.ckpt \
     --input_file data_train/target/$4/claims_test.jsonl \
     --corpus_file data_train/target/$4/corpus.jsonl \
     --output_file ../predictions/xtrain$3_$num_$4.jsonl
 
-#  mv checkpoints_user/$2/checkpoint/last.ckpt /home/multivers/checkpoints/xtrain$3_$num.ckpt
-#  rm -rf checkpoints_user/$2/checkpoint/
-#  mv checkpoints_user/$2 checkpoints_user/xtrain$3_$num
+  mv checkpoints_user/$2/checkpoint/last.ckpt /home/multivers/checkpoints/xtrain$3_$num.ckpt
+  rm -rf checkpoints_user/$2/checkpoint/
+  mv checkpoints_user/$2 checkpoints_user/xtrain$3_$num
 
 }
 
 # Alternate training on citintnm and scifact for 5 epochs each (20 total)
 xtrain5 () {
-#  train_and_predict checkpoints/baseline/healthver.ckpt citintnm 5 citintnm
+  train_and_predict checkpoints/baseline/healthver.ckpt citintnm 5 citintnm
   train_and_predict 1 scifact 5 citintnm
-#  train_and_predict 2 citintnm 5 citintnm
+  train_and_predict 2 citintnm 5 citintnm
   train_and_predict 3 scifact 5 citintnm
 }
 
 # Alternate training on citintnm and scifact for 5 epochs each (20 total)
 xtrain2 () {
-#  train_and_predict checkpoints/baseline/healthver.ckpt citintnm 2 citintnm
+  train_and_predict checkpoints/baseline/healthver.ckpt citintnm 2 citintnm
   train_and_predict 1 scifact 2 citintnm
-#  train_and_predict 2 citintnm 2 citintnm
+  train_and_predict 2 citintnm 2 citintnm
   train_and_predict 3 scifact 2 citintnm
-#  train_and_predict 4 citintnm 2 citintnm
+  train_and_predict 4 citintnm 2 citintnm
   train_and_predict 5 scifact 2 citintnm
-#  train_and_predict 6 citintnm 2 citintnm
+  train_and_predict 6 citintnm 2 citintnm
   train_and_predict 7 scifact 2 citintnm
-#  train_and_predict 8 citintnm 2 citintnm
+  train_and_predict 8 citintnm 2 citintnm
   train_and_predict 9 scifact 2 citintnm
 }
 
 # Alternate training on citintnm and scifact for 1 epoch each (20 total)
 xtrain1 () {
-#  train_and_predict checkpoints/baseline/healthver.ckpt citintnm 1 citintnm
+  train_and_predict checkpoints/baseline/healthver.ckpt citintnm 1 citintnm
   train_and_predict 1 scifact 1 citintnm
-#  train_and_predict 2 citintnm 1 citintnm
+  train_and_predict 2 citintnm 1 citintnm
   train_and_predict 3 scifact 1 citintnm
-#  train_and_predict 4 citintnm 1 citintnm
+  train_and_predict 4 citintnm 1 citintnm
   train_and_predict 5 scifact 1 citintnm
-#  train_and_predict 6 citintnm 1 citintnm
+  train_and_predict 6 citintnm 1 citintnm
   train_and_predict 7 scifact 1 citintnm
-#  train_and_predict 8 citintnm 1 citintnm
+  train_and_predict 8 citintnm 1 citintnm
   train_and_predict 9 scifact 1 citintnm
-#  train_and_predict 10 citintnm 1 citintnm
+  train_and_predict 10 citintnm 1 citintnm
   train_and_predict 11 scifact 1 citintnm
-#  train_and_predict 12 citintnm 1 citintnm
+  train_and_predict 12 citintnm 1 citintnm
   train_and_predict 13 scifact 1 citintnm
-#  train_and_predict 14 citintnm 1 citintnm
+  train_and_predict 14 citintnm 1 citintnm
   train_and_predict 15 scifact 1 citintnm
-#  train_and_predict 16 citintnm 1 citintnm
+  train_and_predict 16 citintnm 1 citintnm
   train_and_predict 17 scifact 1 citintnm
-#  train_and_predict 18 citintnm 1 citintnm
+  train_and_predict 18 citintnm 1 citintnm
   train_and_predict 19 scifact 1 citintnm
 }
 
